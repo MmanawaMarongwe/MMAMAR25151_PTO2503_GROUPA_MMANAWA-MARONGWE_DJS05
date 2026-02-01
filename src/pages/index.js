32b3af84ee8b56aba7bb201d91d65ec35555ffc5
MmanawaMarongwe <140721349+MmanawaMarongwe@users.noreply.github.com>
2026-02-01 13:56:39 +0200

    extract SeasonList component for show details

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as ShowCover } from "./ShowCover";
 export { default as ShowDetailHeader } from "./ShowDetailHeader";
 export {default as ShowInfo} from "./ShowInfo"
+export {default as SeasonList} from "./SeasonList"
